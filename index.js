ac9b80bba9599386fc5634611f6469ead16ecc09
凉凉 <xyqfer@gmail.com>
2019-10-06 15:13:19 +0800

    v1.0.3
    修复部署报错

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ module.exports = ({ framwork = 'express', path = '/deploy',  branch = 'master', 
         deployUrl,
     };
 
-    return (...args) => {
+    return async (...args) => {
         const isExpress = framwork === 'express'; 
         params.request = isExpress ? args[0] : args[0].request;
         const result = deploy(params);
 
         if (!result) {
-            (isExpress ? args[2] : args[1])();
+            await (isExpress ? args[2] : args[1])();
         } else {
             if (isExpress) {
                 args[1].status(200).end();
